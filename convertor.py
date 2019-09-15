50e0b4111d3de8e34a4ab7564b16a26db83a7a76
Shardul Kulkarni <dev.shardulind@gmail.com>
2019-09-16 01:07:43 +0530

    Update convertor.py

--- convertor.py
+++ convertor.py
@@ -4,38 +4,43 @@ import comtypes.client
 from win32com import client
 import win32api
 import docx
-def wordtopdf():
+def wordtopdf(filepath,pdfpath,outputName):
     wdFormatPDF = 17
-    in_file = os.path.abspath('C:\\Users\\Adhiksha\\Desktop\\csv files\\story.docx')
-    out_file = os.path.abspath('C:\\Users\\Adhiksha\\Desktop\\csv files\\out.pdf')
+    in_file = os.path.abspath(filepath)
+    out = str(pdfpath) + outputName + '.pdf'
+    out_file = os.path.abspath(out)
     word = comtypes.client.CreateObject('Word.Application')
     doc = word.Documents.Open(in_file)
     doc.SaveAs(out_file, FileFormat=wdFormatPDF)
     doc.Close()
     word.Quit()
 
-def wordpdf():
+    return out
+
+def wordpdf(filepath, pdfpath, outputName):
     wdFormatPDF = 17
-    in_file = os.path.abspath('C:\\Users\\Adhiksha\\Desktop\\csv files\\document.docx')
-    out_file = os.path.abspath('C:\\Users\\Adhiksha\\Desktop\\csv files\\out.pdf')
+    out = str(pdfpath) + outputName + '.pdf'
+    in_file = os.path.abspath(filepath)
+    out_file = os.path.abspath(out)
     word = comtypes.client.CreateObject('Word.Application')
     doc = word.Documents.Open(in_file)
     doc.SaveAs(out_file, FileFormat=wdFormatPDF)
     doc.Close()
     word.Quit()
+    return out
 
 
 
-
-def exceltopdf():
+def exceltopdf(filepath, pdfpath, outputName):
     excel = client.DispatchEx("Excel.Application")
     excel.Visible = 0
 
-    wb = excel.Workbooks.Open('C:\\Users\\Adhiksha\\Desktop\\csv files\\inner.xlsx')
+    wb = excel.Workbooks.Open(filepath)
     ws = wb.Worksheets[0]
 
+    out = str(pdfpath) + outputName + '.pdf'
     try:
-        wb.SaveAs('C:\\Users\\Adhiksha\\Desktop\\csv files\\file.pdf', FileFormat=57)
+        wb.SaveAs(out, FileFormat=57)
     except Exception (e):
         print ("Failed to convert")
         print (e)
@@ -43,15 +48,20 @@ def exceltopdf():
         wb.Close()
         excel.Quit()
 
-def texttopdf():
-    f=open(r'C:\Users\Adhiksha\Desktop\csv files\codefordfa.txt','r')
+    return out
+
+def texttopdf(filepath, pdfpath, outputName):
+    #f=open(r'C:\Users\Adhiksha\Desktop\csv files\codefordfa.txt','r')
+    f=open(filepath,'r')
+
+    out = str(pdfpath) + outputName + '.pdf'
     text = f.read()
     docu=docx.Document()
     para=docu.add_paragraph(text)
-    docu.save('C:\\Users\\Adhiksha\\Desktop\\csv files\\document.docx')
+    docu.save(out)
     wordpdf()
     f.close()
-
+    return out
 
 
 
